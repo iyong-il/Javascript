f328f93cc1cc22802b395ea2e53b9c8677abf209
iyong-il <lyi1233313@gmail.com>
2023-01-14 20:10:36 +0900

    23.01.14 / JS - 네비바 버튼이벤트

--- portfolio/main.js
+++ portfolio/main.js
@@ -13,3 +13,16 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+// 네비바 메뉴버튼 클릭시 화면전환
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (e) => {
+  const link = e.target.dataset.link;
+  if (link == null) {
+    return;
+  } else {
+    // console.log(e.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior: "smooth" });
+  }
+});
