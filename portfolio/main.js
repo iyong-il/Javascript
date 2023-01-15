c75e40cf6b82fa28215ee053cda9884c8db51156
iyong-il <lyi1233313@gmail.com>
2023-01-15 18:00:27 +0900

    23.01.15 / JS - contactMe 버튼

--- portfolio/main.js
+++ portfolio/main.js
@@ -22,7 +22,18 @@ navbarMenu.addEventListener("click", (e) => {
     return;
   } else {
     // console.log(e.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({ behavior: "smooth" });
+    scrollIntoView(link);
   }
 });
+
+// contact me 버튼 눌렀을 때 화면 전환
+const contactMe = document.querySelector(".home__contact");
+contactMe.addEventListener("click", () => {
+  scrollIntoView("#contact");
+});
+
+// 화면전환 메서드
+const scrollIntoView = (selector) => {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+};
