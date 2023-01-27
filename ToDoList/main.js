079fc5ffe181a7900a1a5b8b52e255bdce07b19b
iyong-il <lyi1233313@gmail.com>
2023-01-27 23:29:47 +0900

    23.01.27 / ToDoList최종완성

--- ToDoList/main.js
+++ ToDoList/main.js
@@ -2,6 +2,42 @@ const taskInput = document.querySelector(".input-area");
 const add = document.querySelector(".plus-button");
 let taskList = [];
 let contents = document.querySelector(".contents__row");
+let tabs = document.querySelectorAll(".menu .menu__item");
+let filterList = [];
+let buttonID = "all";
+
+// 렌더링해주는 메서드
+const render = () => {
+  let copyList = [];
+  if (buttonID === "all") {
+    copyList = taskList;
+  } else if (buttonID === "going" || buttonID === "done") {
+    copyList = filterList;
+  }
+
+  let result = "";
+  for (let i = 0; i < copyList.length; i++) {
+    if (copyList[i].isComplete === true) {
+      result += `<div class="contents">
+      <div class="task__contents" style="text-decoration:line-through;">${copyList[i].taskContents}</div>
+      <div class="button">
+        <button onclick="toggleCheck('${copyList[i].id}')">check</button>
+        <button onclick="deleteButton('${copyList[i].id}')">delete</button>
+      </div>
+    </div>`;
+    } else {
+      result += `<div class="contents">
+    <div class="task__contents">${copyList[i].taskContents}</div>
+    <div class="button">
+      <button onclick="toggleCheck('${copyList[i].id}')">check</button>
+      <button onclick="deleteButton('${copyList[i].id}')">delete</button>
+    </div>
+  </div>`;
+    }
+  }
+
+  contents.innerHTML = result;
+};
 
 // 플러스버튼을 눌렀을 때
 const addTask = () => {
@@ -22,7 +58,7 @@ const addTask = () => {
 // check버튼 클릭시
 const toggleCheck = (e) => {
   console.log(e);
-  for (i = 0; i < taskList.length; i++) {
+  for (let i = 0; i < taskList.length; i++) {
     if (taskList[i].id === e) {
       // toggle
       taskList[i].isComplete = !taskList[i].isComplete;
@@ -44,30 +80,33 @@ const deleteButton = (e) => {
   }
 };
 
-// 렌더링해주는 메서드
-const render = () => {
-  let result = "";
-  for (let i = 0; i < taskList.length; i++) {
-    if (taskList[i].isComplete === true) {
-      result += `<div class="contents">
-      <div class="task__contents" style="text-decoration:line-through;">${taskList[i].taskContents}</div>
-      <div class="button">
-        <button onclick="toggleCheck('${taskList[i].id}')">check</button>
-        <button onclick="deleteButton('${taskList[i].id}')">delete</button>
-      </div>
-    </div>`;
-    } else {
-      result += `<div class="contents">
-    <div class="task__contents">${taskList[i].taskContents}</div>
-    <div class="button">
-      <button onclick="toggleCheck('${taskList[i].id}')">check</button>
-      <button onclick="deleteButton('${taskList[i].id}')">delete</button>
-    </div>
-  </div>`;
-    }
-  }
+for (i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", (e) => {
+    filter(e);
+  });
+}
 
-  contents.innerHTML = result;
+const filter = (e) => {
+  console.log(e.target.id);
+  buttonID = e.target.id;
+  filterList = [];
+  if (buttonID === "all") {
+    render();
+  } else if (buttonID === "going") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete === false) {
+        filterList.push(taskList[i]);
+      }
+    }
+    render();
+  } else if (buttonID === "done") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete === true) {
+        filterList.push(taskList[i]);
+      }
+    }
+    render();
+  }
 };
 
 add.addEventListener("click", addTask);
